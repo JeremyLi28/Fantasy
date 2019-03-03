c01ad573a34a42e118e17da51904ebc3395f0b85
JeremyLi28 <270768253@qq.com>
2019-03-03 11:49:43 -0800

    change points and position depend on slate

--- data_extractor.py
+++ data_extractor.py
@@ -7,16 +7,17 @@ home = './'
 def Extract(year, month, day):
 	player_path = home + 'data/crawler/rotogrinders/projections/%s-%s-%s.json' % (year, month, day)
 	player_json_data = json.loads(open(player_path, 'r').read())
-	player_dict = {'name' : [], 'position' : [], 'slate_id' : [], 'player_id': [], 'salary' : [], 'points' : []}
+	player_dict = {'name' : [], 'position' : [], 'slate_id' : [], 'type' : [], 'player_id' : [], 'salary' : [], 'points' : []}
 	for data in player_json_data:
 		if data['import_data'] is None:
 			print 'None import_data for ' + data['player_name']
 			continue
 		for slate in data['import_data']:
 			player_dict['name'].append(data['player_name'])
-			player_dict['position'].append(data['position'])
-			player_dict['points'].append(data['points'])
+			player_dict['position'].append(slate['position'])
+			player_dict['points'].append(slate['fpts'])
 			player_dict['slate_id'].append(slate['slate_id'])
+			player_dict['type'].append(slate['type'])
 			player_dict['player_id'].append(slate['player_id'])
 			player_dict['salary'].append(slate['salary'])
 	player_df = pd.DataFrame.from_dict(player_dict)
