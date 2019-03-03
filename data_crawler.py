8a060390dbee260f460e2e97107004171670ca01
JeremyLi28 <jeremyliisagoodman@gmail.com>
2019-03-02 18:11:15 -0800

    add slate: crawler & extractor change

--- data_crawler.py
+++ data_crawler.py
@@ -5,16 +5,21 @@ import json
 
 home = './'
 
-def fetch_roto_grinders_proj(year, month, day):
+def Crawl(year, month, day):
 	url = 'https://rotogrinders.com/projected-stats/nba-player?site=draftkings&sport=nba&date=%s-%s-%s' % (year, month, day)
 	r = urllib.urlopen(url).read()
 	soup = BeautifulSoup(r, features="html.parser")
 	# Have 2 scripts tag, we use the 2nd for now, the usage of first need to be further investigated, related to vegas money line
 	script_tags = soup.find('section', {'class' : 'pag bdy'}).findAll('script')
-	raw_data = script_tags[2].text.split('\n')[2].strip().split('=')[1].strip()[:-1]
-	json_data = json.loads(raw_data)
-	with open(home + 'data/raw/rotogrinders/projections/%s-%s-%s.json' % (year, month, day), 'w') as json_file:
-		json.dump(json_data, json_file)
+	player_data = script_tags[2].text.split('\n')[2].strip().split('=')[1].strip()[:-1]
+	slate_data = script_tags[1].text.split('\n')[4].strip().split('slates: ')[1][:-1]
+	schedule_data = script_tags[1].text.split('\n')[5].strip().split('schedules: ')[1][:-1]
+	player_json_data = json.loads(player_data)
+	slate_json_data = json.loads(slate_data)
+	with open(home + 'data/crawler/rotogrinders/slates/%s-%s-%s.json' % (year, month, day), 'w') as slate_json_file:
+		json.dump(slate_json_data, slate_json_file)
+	with open(home + 'data/crawler/rotogrinders/projections/%s-%s-%s.json' % (year, month, day), 'w') as player_json_file:
+		json.dump(player_json_data, player_json_file)
 	print "fetch_roto_grainders_proj for %s-%s-%s" % (year, month, day)
 
 
@@ -22,4 +27,4 @@ if __name__ == "__main__":
 	year = sys.argv[1]
 	month = sys.argv[2]
 	day = sys.argv[3]
-	fetch_roto_grinders_proj(year, month, day)
+	Crawl(year, month, day)
--- data_extractor.py
+++ data_extractor.py
@@ -4,22 +4,39 @@ import sys
 
 home = './'
 
-def load_roto_grinders_proj(year, month, day):
-	path = home + 'data/raw/rotogrinders/projections/%s-%s-%s.json' % (year, month, day)
-	json_data = json.loads(open(path, 'r').read())
-	rg_dict = {'name' : [], 'position' : [], 'salary' : [], 'points' : []}
-	for data in json_data:
-		rg_dict['name'].append(data['player_name'])
-		rg_dict['position'].append(data['position'])
-		rg_dict['salary'].append(data['salary'])
-		rg_dict['points'].append(data['points'])
-	rg_df = pd.DataFrame.from_dict(rg_dict)
-	rg_df.set_index('name', inplace=True)
-	rg_df.to_csv(home + 'data/extractor/rotogrinders_proj/%s-%s-%s.csv' % (year, month, day))
+def Extract(year, month, day):
+	player_path = home + 'data/crawler/rotogrinders/projections/%s-%s-%s.json' % (year, month, day)
+	player_json_data = json.loads(open(player_path, 'r').read())
+	player_dict = {'name' : [], 'position' : [], 'salary' : [], 'points' : []}
+	player_slate_id_dict = {'player_name' : [], 'slate_id' : [], 'player_id': []}
+	for data in player_json_data:
+		player_dict['name'].append(data['player_name'])
+		player_dict['position'].append(data['position'])
+		player_dict['salary'].append(data['salary'])
+		player_dict['points'].append(data['points'])
+		for slate in data['import_data']:
+			player_slate_id_dict['player_name'].append(data['player_name'])
+			player_slate_id_dict['slate_id'].append(slate['slate_id'])
+			player_slate_id_dict['player_id'].append(slate['player_id'])
+	player_df = pd.DataFrame.from_dict(player_dict)
+	player_df.set_index('name', inplace=True)
+	player_df.to_csv(home + 'data/extractor/rotogrinders/projections/%s-%s-%s.csv' % (year, month, day))
+	player_slate_id_df = pd.DataFrame.from_dict(player_slate_id_dict)
+	player_slate_id_df.set_index('player_name', inplace=True)
+	player_slate_id_df.to_csv(home + 'data/extractor/rotogrinders/player_slate_id/%s-%s-%s.csv' % (year, month, day))
 
+	slate_path = home + 'data/crawler/rotogrinders/slates/%s-%s-%s.json' % (year, month, day)
+	slate_json_data = json.loads(open(slate_path, 'r').read())
+	slate_dict = {'name' : [], 'id' : []}
+	for key, value in slate_json_data.iteritems():
+		slate_dict['name'].append(key);
+		slate_dict['id'].append(value['importId'] if key != 'All Games' else '0')
+	slate_df = pd.DataFrame.from_dict(slate_dict)
+	slate_df.set_index('name', inplace=True)
+	slate_df.to_csv(home + 'data/extractor/rotogrinders/slates/%s-%s-%s.csv' % (year, month, day))
 
 if __name__ == "__main__":
 	year = sys.argv[1]
 	month = sys.argv[2]
 	day = sys.argv[3]
-	load_roto_grinders_proj(year, month, day)
+	Extract(year, month, day)
