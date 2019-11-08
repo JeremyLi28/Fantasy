150135985393885e89a54e41d10c95c06590691a
JeremyLi28 <jeremyliisagoodman@gmail.com>
2019-11-07 17:40:47 -0800

    add basic package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jiayin',
+      version='0.1',
+      description='Fantasy basketball toolkits',
+      url='https://github.com/JeremyLi28/Fantasy',
+      author='JeremyLi',
+      author_email='jeremyliisagoodman@gmail.com',
+      license='MIT',
+      packages=['jiayin'],
+      zip_safe=False)
