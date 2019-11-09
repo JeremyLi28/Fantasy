86cf6d78cb57db7ffb6f46e440c30a59fac28267
JeremyLi28 <jeremyliisagoodman@gmail.com>
2019-11-08 22:48:41 -0800

    fix cronjob

--- fantasy-basketball-toolkit/__init__.py
+++ fantasy-basketball-toolkit/__init__.py
@@ -1,7 +1,6 @@
 from crontab import CronTab
 
-cron = CronTab()
+cron = CronTab(user=True)
 job = cron.new(command='python3 ../data_crawler.py -t \'DK\' ~/install.log 2>&1')
-job.minute.every(1)
-
+job.hour.every(12)
 cron.write()
