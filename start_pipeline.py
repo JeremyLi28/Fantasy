45605f56a700ecd69fca1cea310aa9340f026ca3
JeremyLi28 <jeremyliisagoodman@gmail.com>
2019-11-09 22:18:26 -0800

    add support to run script in absolute directory

--- start_pipeline.py
+++ start_pipeline.py
@@ -1,6 +1,13 @@
 from crontab import CronTab
+import os
+
+cwd = os.getcwd()
 
 cron = CronTab(user=True)
-job = cron.new(command='python3 $(pwd)/data_crawler.py -t \'DK\' ~/install.log 2>&1')
+job = cron.new(command='python3 %s/fantasy-basketball-toolkit/data_crawler.py -t \'DK\' ~/install.log 2>&1' % cwd)
 job.hour.every(12)
+
+for item in cron:
+    print item
+
 cron.write()
