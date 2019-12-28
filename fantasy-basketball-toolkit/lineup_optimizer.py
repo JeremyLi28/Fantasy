974f22c5bfc85b6e93e70e20c4839136314e4f1e
JeremyLi28 <jeremyliisagoodman@gmail.com>
2019-12-27 16:13:39 -0800

    lineup optimizer bug fix

--- fantasy-basketball-toolkit/lineup_optimizer.py
+++ fantasy-basketball-toolkit/lineup_optimizer.py
@@ -5,7 +5,9 @@ import heapq
 import os
 from optparse import OptionParser
 import datetime
-import draftfast
+from draftfast import rules
+from draftfast.optimize import run
+from draftfast.orm import Player
 from utils import *
 
 def Optimize(date, top_k, slate_id, projection_source):
@@ -22,7 +24,7 @@ def Optimize(date, top_k, slate_id, projection_source):
 	slates = pd.read_csv(slates_path, header = 0, index_col = 0)
 
 	if slate_id not in slates['SLATE_ID'].tolist():
-		print "slate %d not exist!" % slate_id
+		print("slate %d not exist!" % slate_id)
 		return
 	if slates[slates['SLATE_ID'] == slate_id]['SLATE_TYPE'].values[0] != 'CLASSIC':
 		print("slate type %s not supported!" % slate_type)
@@ -33,15 +35,22 @@ def Optimize(date, top_k, slate_id, projection_source):
 	players = players[players['SLATE_ID'] == slate_id]
 	players.set_index('NAME', inplace=True)
 
-	players_with_projection = players.join(projections)
+	players_with_projection = projections.join(players, how='inner')
 
 	player_pool = []
-	for name, data in players_with_projection.iterrows()::
-		player_pool.append(Player(name = name, cost = data['SALARY'], proj = data['points']))
+	for name, data in players_with_projection.iterrows():
+		for pos in data['POSITION'].split('/'):
+			player_pool.append(Player(name = name, cost = data['SALARY'], proj = data['PROJECTION'], pos = pos))
+
+	roster = run(
+		rule_set=rules.DK_NBA_RULE_SET,
+		player_pool=player_pool,
+		verbose=True,
+	)
 
 if __name__ == "__main__":
 	parser = OptionParser()
-	parser.add_option("-d", "--date", dest="date", default=datetime.datetime.today().strftime('%Y-%m-%d'))
+	parser.add_option("-d", "--date", dest="date", default=datetime.today().astimezone(pytz.timezone('America/Los_Angeles')).strftime('%Y-%m-%d'))
 	parser.add_option("-k", "--top_k", dest="top_k", default=2)
 	parser.add_option("-s", "--slate", dest="slate_id")
 	(options, args) = parser.parse_args()
